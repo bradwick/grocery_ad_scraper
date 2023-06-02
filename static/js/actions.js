9e28d5f6f5d65902d23cfc8cc5692013fcf90ab8
Brad Neuroth <neurothbr@epb.net>
2023-06-01 21:00:23 -0400

    try to make the page better to use

---
+++ static/js/actions.js
@@ -0,0 +1,36 @@
+function listenerOnSelector (selector, eventType, myFunction) {
+    document.querySelectorAll(selector).forEach((item) => {
+        item.addEventListener(eventType, (event) => myFunction(event))
+    })
+}
+
+listenerOnSelector(".hide", "click", e =>{
+    const id = e.target.closest("button").dataset.id
+    fetch("/hide", {
+        body: JSON.stringify({id:id})
+    })
+    e.target.closest(".deal-row").classList.add("hidden")
+})
+
+listenerOnSelector(".pin", "click", e =>{
+    const button = e.target.closest("button")
+    const id = button.dataset.id
+    fetch("/favorite", {
+        body: JSON.stringify({id:id})
+    })
+    if (button.alt === "Un-Favorite")
+        button.querySelector("img").src = '/static/img/unpin.png'
+    else
+        button.querySelector("img").src = '/static/img/pin.png'
+})
+
+listenerOnSelector(".save", "click", e =>{
+    const id = e.target.closest("button").dataset.id
+    fetch("/add", {
+        body: JSON.stringify({id:id})
+    })
+    if (button.alt === "Un-Save")
+        button.querySelector("img").src = '/static/img/remove.png'
+    else
+        button.querySelector("img").src = '/static/img/add.png'
+})
